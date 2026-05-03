474d2a42683edc4c33187ee17e0cc9942884222b
miguelpm-bio <priormembrives@gmail.com>
2026-05-03 12:12:16 +0200

    Counting DNA Nucleotides

---
+++ stronghold/DNA.py
@@ -0,0 +1,6 @@
+dna_string = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
+count_a = dna_string.count('A')
+count_c = dna_string.count('C')
+count_g = dna_string.count('G')
+count_t = dna_string.count('T')
+print(count_a, count_c, count_g, count_t)
